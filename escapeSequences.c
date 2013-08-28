9728e9fc9d290a31c51e3d479405b6010d646c6a
Karan <karan4c6@gmail.com>
2013-08-28 23:32:59 +0530

    escape sequences in C

---
+++ escapeSequences.c
@@ -0,0 +1,43 @@
+  /**  
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    * 
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    * 
+    * Copyright 2013 Maven Scientists http://mavenscientists.com
+    */
+
+ /**
+   * @author	Maven Scientists (http://mavenscientists.com) 
+   * @email	info@mavenscientists.com
+   * Escape Sequences - This program shows all the escape sequesnces available in C Language
+   * Use all the escape sequences one by one in order to know the task of each.
+   */
+
+#include <stdio.h>
+
+int main()
+{
+	
+/*	All escape sequences
+	\a - alert sound
+	\b - backspace
+	\r - carriage return
+	\0 - null character
+	\n - new line
+	\t - horizontal tab
+	\\ - print backslash
+	\" - print double quote
+	\' - print single quote
+*/
+
+	printf("Hello World \a");
+/* %% - print % sign
+*/
+	return 0;
+}
